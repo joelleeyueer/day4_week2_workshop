1b6e993034fc7d362f8f1f1711d7b571f932a562
joelsiberia <35606477+joelleeyueer@users.noreply.github.com>
2023-01-17 14:18:40 +0800

    Added Cookie Class

---
+++ src/main/java/nus/iss/Cookie.java
@@ -0,0 +1,48 @@
+package nus.iss;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Cookie {
+    String dirPath = "./data2";
+    String fileName = "cookie.txt";
+
+    List<String> cookieItems;
+
+    public void readCookieFile() throws IOException {
+        cookieItems = new ArrayList<>();
+
+        File file = new File(dirPath + File.separator + fileName);
+
+        FileReader fr = new FileReader(file);
+        BufferedReader br = new BufferedReader(fr);
+
+        String incomingString;
+
+        while (null != (incomingString = br.readLine())){
+            cookieItems.add(incomingString);
+        }
+    }
+
+    public String returnCookie(){
+        Random random = new Random();
+
+        if (cookieItems != null){
+            return cookieItems.get((random.nextInt(cookieItems.size())));
+        } else {
+            return "No cookies found :(";
+        }
+    }
+
+    public void showCookies(){
+        if (cookieItems == null) {
+            System.err.println("No cookies found :(");
+        } else {
+            for (String cookie:cookieItems){
+                System.out.println(cookie);
+            }
+        }
+    }
+}
--- src/main/java/nus/iss/Main.java
+++ src/main/java/nus/iss/Main.java
@@ -1,7 +1,23 @@
 package nus.iss;
 
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Cookie c = new Cookie();
+
+
+
+
+    }
+
+    public void checkDirectory(String directoryPath){
+        File newDirectory = new File(directoryPath);
+        if (newDirectory.exists()){
+            System.out.println("Directory already exists!");
+        } else {
+            System.out.println("Created the directory!");
+            newDirectory.mkdir();
+        }
     }
 }
